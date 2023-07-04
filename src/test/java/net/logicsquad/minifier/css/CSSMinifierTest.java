695ac03187b0b6558836385ba4c70179d2d773da
Paul Hoadley <paulh@logicsquad.net>
2023-07-04 17:44:34 +1000

    #2 Adds unit tests for problematic input.
    The issue here is the omitted trailing semicolon, followed by no
    whitespace (other than the newline) and then the closing
    brace. CSSMinifier assumes it can safely truncate by two characters on
    the trailing end, because the first will be the brace, followed by the
    semicolon. And if there's whitespace instead, the issue doesn't
    manifest. In these cases, though, it truncates a character from the
    property's content.

--- src/test/java/net/logicsquad/minifier/css/CSSMinifierTest.java
+++ src/test/java/net/logicsquad/minifier/css/CSSMinifierTest.java
@@ -17,7 +17,7 @@ public class CSSMinifierTest extends AbstractMinifierTest {
 	 * Indexes for input/output resources
 	 */
 	private static final List<String> RESOURCES = Arrays.asList("01", "02", "03", "04", "05", "06", "07", "08", "09",
-			"10", "11", "12", "13", "14", "15", "16", "17", "18");
+			"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21");
 
 	/**
 	 * Extension for resource files
